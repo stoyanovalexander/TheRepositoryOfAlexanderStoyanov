b86ee23888d006e24a45a2d2ae96b1f33ae3108c
Alexander Stoyanov <stoyanovalexander1986@yahoo.com>
2013-02-18 10:08:33 +0100

    Numeral Systems
    The Home work NumeralSystems in Telerik

---
+++ 05.NumeralSystems/03.DecimalToHexadecimal/DecimalToHexadecimal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+    class DecimalToHexadecimal
+    {
+        static void Main()
+        {
+            Console.Write("             Enter DECIMAL number: ");
+            int decimalNum = int.Parse(Console.ReadLine());
+            List<string> hexDigits = new List<string>();
+            int hexSwitch = 0;
+            // Fill the List of hexDigits
+            while (decimalNum != 0)
+            {
+                hexSwitch = decimalNum % 16;
+                decimalNum = decimalNum / 16;
+                switch (hexSwitch)
+                { 
+                    case 0:
+                        hexDigits.Add("0");break;
+                    case 1:
+                        hexDigits.Add("1"); break;
+                    case 2:
+                        hexDigits.Add("2"); break;
+                    case 3:
+                        hexDigits.Add("3"); break;
+                    case 4:
+                        hexDigits.Add("4"); break;
+                    case 5:
+                        hexDigits.Add("5"); break;
+                    case 6:
+                        hexDigits.Add("6"); break;
+                    case 7:
+                        hexDigits.Add("7"); break;
+                    case 8:
+                        hexDigits.Add("8"); break;
+                    case 9:
+                        hexDigits.Add("9"); break;
+                    case 10:
+                        hexDigits.Add("A"); break;
+                    case 11:
+                        hexDigits.Add("B"); break;
+                    case 12:
+                        hexDigits.Add("C"); break;
+                    case 13:
+                        hexDigits.Add("D"); break;
+                    case 14:
+                        hexDigits.Add("E"); break;
+                    case 15:
+                        hexDigits.Add("F"); break;
+                    default:
+                        Console.WriteLine("There must have some mistake!!!");; break;
+                }
+            }
+            Console.Write("The hexadecimal representation is: ");
+            // Print the result
+            for (int i = hexDigits.Count - 1; i >= 0; i--)
+            {
+                Console.Write(hexDigits[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+
---
+++ 05.NumeralSystems/05.HexadecimalToBinary/HexadecimalToBinary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+    class HexadecimalToBinary
+    {
+        static void Main()
+        {
+            Console.Write("    Enter HEXADECIMAL number: ");
+            string hexNumber = Console.ReadLine();
+            Console.Write("The BINARY representation is: ");
+            // Print the result
+            for(int i=0;i<hexNumber.Length;i++)
+            {
+                switch (hexNumber[i])
+                { 
+                    case '0':
+                        Console.Write("0000");break;
+                    case '1':
+                        Console.Write("0001"); break;
+                    case '2':
+                        Console.Write("0010"); break;
+                    case '3':
+                        Console.Write("0011"); break;
+                    case '4':
+                        Console.Write("0100"); break;
+                    case '5':
+                        Console.Write("0101"); break;
+                    case '6':
+                        Console.Write("0110"); break;
+                    case '7':
+                        Console.Write("0111"); break;
+                    case '8':
+                        Console.Write("1000"); break;
+                    case '9':
+                        Console.Write("1001"); break;
+                    case 'A':
+                    case 'a':
+                        Console.Write("1010"); break;
+                    case 'B':
+                    case 'b':
+                        Console.Write("1011"); break;
+                    case 'C':
+                    case 'c':
+                        Console.Write("1100"); break;
+                    case 'D':
+                    case 'd':
+                        Console.Write("1101"); break;
+                    case 'E':
+                    case 'e':
+                        Console.Write("1110"); break;
+                    case 'F':
+                    case 'f':
+                        Console.Write("1111"); break;
+                    default:
+                        Console.WriteLine("Ther must have some mistake!!!"); break;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
